49487aa0739f030cb229611287d600fcc557c05c
Pradd <pradd.me@gmail.com>
2013-02-04 20:12:44 +0300

    add 'translate' function to Dna module

--- src/Dna.hs
+++ src/Dna.hs
@@ -1,6 +1,19 @@
-module Dna (DNA (..), Gene (..), Score) where 
+module Dna (DNA (..), Gene (..), Score, translate, genes) where 
 
 data DNA = DNA [Gene]
-data Gene = ADD1 | SUB1 | ADD5 | SUB5 deriving (Read, Show)
+data Gene = ADD1 | SUB1 | ADD5 | SUB5 deriving (Read, Show, Eq)
+
+translate :: Gene -> Int -> Int
+translate gene = snd $ head $ filter ((gene ==) . fst) opsMap
+
+genes :: [Gene]
+genes = map fst opsMap
+
+opsMap :: [(Gene, Int -> Int)]
+opsMap = [ (ADD1, \x -> x + 1)
+         , (SUB1, \x -> x - 1)
+         , (ADD5, \x -> x + 5)
+         , (SUB5, \x -> x - 5)
+         ] 
 
 type Score = Int
--- src/Processor.hs
+++ src/Processor.hs
@@ -1,12 +1,9 @@
 module Processor (process) where
 
-import Dna
+import Dna (DNA(..), Gene, translate)
 
 process :: Int -> DNA -> Int
 process x (DNA dna) = foldl performOp x dna
 
 performOp :: Int -> Gene -> Int
-performOp x ADD1 = x + 1
-performOp x ADD5 = x + 5
-performOp x SUB1 = x - 1
-performOp x SUB5 = x - 5
+performOp x gene = (translate gene) x
